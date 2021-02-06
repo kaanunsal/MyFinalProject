02760e2fd0d24796c0ca6252782cc3f6ce62fe21
Kaan Ünsal <kunsalll@outlook.com>
2021-02-07 00:19:00 +0300

    07 şubat ders sonu

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,5 +1,7 @@
-﻿using DataAccess.Concrete.EntityFramework;
+﻿using Core.DataAccess;
+using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +10,8 @@ namespace DataAccess.Abstract
 {
     public interface IProductDal : IEntityRepository<Product>
     {
-    
+        List<ProductDetailDto> GetProductDetails();
     }
 }
+
+//Code Refactoring
