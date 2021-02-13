4296d8bb2c6ac38d6f3d2bfcb988dae7ec00ce8c
Kaan Ünsal <kunsalll@outlook.com>
2021-02-13 23:56:14 +0300

    13 Şubat Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -19,8 +19,7 @@ namespace Business.Concrete
         public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
-        }
-
+        }    
         public IResult Add(Product product)
         {
             //business codes
@@ -37,7 +36,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-           if(DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
